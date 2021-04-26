8aeb8048f9bb7e1f8139eddfc21ce86eabee57cd
joser <joser>
2021-04-26 16:16:29 +0800

    修改了依赖的地址

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "http://pineal.ai:30000/pineal-ios/sub-modules/other/Alamofire.git", from: "5.2.2"),
-        .package(url: "http://pineal.ai:30000/pineal-ios/sub-modules/other/CleanJSON.git", from: "1.0.4")
+        .package(url: "http://gitlab.synology.me/app/Alamofire.git", from: "5.2.2"),
+        .package(url: "http://gitlab.synology.me/app/CleanJSON.git", from: "1.0.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
